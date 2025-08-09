4aa7da6c26754f262840c0c2ddcbf8fedb71d97a
Kevin Axel <carameloreocantugarcia@gmail.com>
2025-08-08 20:08:37 -0600

    added carousel to home needs improvement

---
+++ src/data/carousel-data.tsx
@@ -0,0 +1,23 @@
+import {
+    SiReact,
+    SiTailwindcss,
+    SiNextdotjs,
+    SiNodedotjs,
+    SiMongodb,
+    SiHtml5,
+    SiTypescript,
+  } from "react-icons/si";
+  
+  export const techWeb = [
+    { name: "HTML", icon: <SiHtml5 className="text-orange-500 w-10 h-10" /> },
+    { name: "TypeScript", icon: <SiTypescript className="text-blue-500 w-10 h-10" /> },
+      { name: "React", icon: <SiReact className="text-blue-500 w-10 h-10" /> },
+    { name: "Next.js", icon: <SiNextdotjs className="text-black w-10 h-10" /> },
+    { name: "Tailwind ", icon: <SiTailwindcss className="text-cyan-500 w-10 h-10" /> },
+      { name: "Node.js", icon: <SiNodedotjs className="text-green-600 w-10 h-10" /> },
+    { name: "MongoDB", icon: <SiMongodb className="text-green-700 w-10 h-10" /> },
+  ];
+  export const techMovil = [
+
+  ]
+  
