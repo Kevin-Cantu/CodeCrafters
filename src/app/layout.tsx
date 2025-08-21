1696a1a97fbac14d39fdab60883509a9964a8bda
Kevin Axel <carameloreocantugarcia@gmail.com>
2025-08-20 19:06:17 -0600

    quit scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
-'use client' // necesario para usar useEffect
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import '@/styles/globals.css'
+import { Header } from '@/components/layout/Header'
+import { Footer } from '@/components/layout/Footer'
+import { RouteTransition } from '@/components/layout/RouteTransition'
 
-import { useEffect } from 'react';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import '@/styles/globals.css';
-import { Header } from '@/components/layout/Header';
-import { Footer } from '@/components/layout/Footer';
-import { RouteTransition } from '@/components/layout/RouteTransition';
+const inter = Inter({ subsets: ['latin'] })
 
-const inter = Inter({ subsets: ['latin'] });
-
- const metadata: Metadata = {
+export const metadata: Metadata = {
   title: 'Consultora de Programación | Desarrollo de Software',
   description: 'Consultora especializada en desarrollo de software y frameworks modernos. Creamos soluciones tecnológicas innovadoras para tu negocio.',
   keywords: 'consultora, programación, desarrollo, software, frameworks, react, next.js',
@@ -27,31 +24,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
-  useEffect(() => {
-    let startY = 0;
-  
-    const handleTouchStart = (e: TouchEvent) => {
-      startY = e.touches[0].clientY;
-    };
-  
-    const handleTouchMove = (e: TouchEvent) => {
-      e.preventDefault();
-      const currentY = e.touches[0].clientY;
-      const deltaY = startY - currentY;
-      window.scrollBy({ top: deltaY * .65, left: 0 });
-      startY = currentY;
-    };
-  
-    window.addEventListener('touchstart', handleTouchStart, { passive: false });
-    window.addEventListener('touchmove', handleTouchMove, { passive: false });
-  
-    return () => {
-      window.removeEventListener('touchstart', handleTouchStart);
-      window.removeEventListener('touchmove', handleTouchMove);
-    };
-  }, []);
-
   return (
     <html lang="es">
       <body className={inter.className}>
